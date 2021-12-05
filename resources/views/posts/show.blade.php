130ec5ccb65cb1cfe6bdbf3e2fe7c49a10ab9ba6
natsukiiiii <w.natsuki.3@gmail.com>
2021-12-05 11:58:18 +0900

    feature/#45-destroy

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -9,7 +9,16 @@
             <div class="card-body">
                 <p class="card-text">内容：{{ $post->text }}</p>
                 <p>投稿日時：{{ $post->created_at }}</p>
+
+             <div class="d-flex flex-row">
                 <a href="{{route('posts.edit', $post->id)}}" class="btn btn-primary">編集</a>
+                <form action="{{route('posts.destroy', $post->id)}}" method="post">
+                  {{ csrf_field() }}
+                  {{ method_field('DELETE') }}
+                  <input type="submit" value="削除" class="btn btn-light border" onclick='return confirm("本当に削除しますか？？")'>
+                </form>
+             </div>
+
             </div>
         </div>
     </div>
