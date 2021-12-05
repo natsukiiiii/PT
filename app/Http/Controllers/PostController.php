a1044bdc3c72ee38f6c055ce67b7b02c15ca4b3f
natsukiiiii <w.natsuki.3@gmail.com>
2021-12-05 12:09:34 +0900

    feature/#52-username add username

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -22,6 +22,7 @@ class PostController extends Controller
     public function index()
     {
         $posts = Post::all();
+        $posts->load('user');
         return view('posts.index', compact('posts'));
     }
 
