f912a642f229085bcdc68434349d5d4f8e540678
natsukiiiii <w.natsuki.3@gmail.com>
2021-12-09 17:16:51 +0900

    education edit

---
+++ resources/views/educations/edit.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.app')
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    {{-- @dd($experience); --}}
+    @section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+
+                <div class="card text-center">
+                    <div class="card-body">
+                        <h3>学歴</h3>
+                        <div class="card-body">
+                            {{-- <h5 class="card-title">経験分野 : {{ $experience->name }}</h5> --}}
+
+                        </div>
+
+
+                            <form action="{{ route('education.update', $education->id) }}" method="post">
+                                {{csrf_field()}}
+                                {{method_field('PATCH')}}
+                                <p>学歴</p>
+                                <textarea name="name" value="{{ $education->name }}" placeholder="ex) OO大学病院, リハビリテーション科、整形外来" cols="30" rows="10">
+                                    {{ $education->name }}
+                                </textarea>
+
+                            <br><button type="submit" class="btn btn-primary">編集する</button>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    @endsection
+</body>
+
+</html>
