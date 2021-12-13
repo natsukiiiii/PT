b2ac4aa0bdafa4f3f8bf1a60a568f8b2da3c8071
natsukiiiii <w.natsuki.3@gmail.com>
2021-12-13 12:28:02 +0900

    search

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -19,12 +19,46 @@ class PostController extends Controller
     }
 
 
-    public function index()
+    public function index(Request $request)
     {
-        $posts = Post::all();
+        // $posts = Post::all();28でもあるので上書きされる
+
+        // return view('posts.index', compact('posts'));
+// ユーザー一覧をページネートで取得
+$posts = Post::paginate(20);
+// 検索フォームで入力された値を取得する
+         $search = $request->input('search');
+        // クエリビルダ
+        $query = Post::query();
+
+       // もし検索フォームにキーワードが入力されたら
+        if ($search !== null) {
+
+            // 全角スペースを半角に変換
+            $spaceConversion = mb_convert_kana($search, 's');
+
+            // 単語を半角スペースで区切り、配列にする（例："山田 翔" → ["山田", "翔"]）
+            $wordArraySearched = preg_split('/[\s,]+/', $spaceConversion, -1, PREG_SPLIT_NO_EMPTY);
+
+
+            // 単語をループで回し、ユーザーネームと部分一致するものがあれば、$queryとして保持される
+            foreach($wordArraySearched as $value) {
+                $query->where('title', 'like', '%'.$value.'%')->orWhere('text', 'like', '%'.$value.'%');
+            }
+// 上記で取得した$queryをページネートにし、変数$usersに代入
+// getでも良い
+            $posts = $query->paginate(20);
+        }
         $posts->load('user');
-        return view('posts.index', compact('posts'));
+// dd($search);
+        // ビューにpostsとsearchを変数として渡す
+        return view('posts.index')
+            ->with([
+                'posts' => $posts,
+                'search' => $search,
+            ]);
     }
+
 
     /**
      * Show the form for creating a new resource.
