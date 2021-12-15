8e855ba4723b52f5648fced9ebe3919950bda689
natsukiiiii <w.natsuki.3@gmail.com>
2021-12-15 16:11:00 +0900

    education edit,index view

--- resources/views/educations/edit.blade.php
+++ resources/views/educations/edit.blade.php
@@ -11,16 +11,17 @@
     <title>Document</title>
 </head>
 
-<body>
+<body class="bg-white">
     {{-- @dd($experience); --}}
     @section('content')
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
 
-                <div class="card text-center">
+                <div class="text-center">
                     <div class="card-body">
-                        <h3>学歴</h3>
+                        <h3 style="margin-top:40px; font-size:34px;"
+                        class="text-left font-weight-bold">学歴</h3>
                         <div class="card-body">
                             {{-- <h5 class="card-title">経験分野 : {{ $experience->name }}</h5> --}}
 
@@ -30,12 +31,14 @@
                             <form action="{{ route('education.update', $education->id) }}" method="post">
                                 {{csrf_field()}}
                                 {{method_field('PATCH')}}
-                                <p>学歴</p>
-                                <textarea name="name" value="{{ $education->name }}" placeholder="ex) OO大学, リハビリテーション科" cols="30" rows="10">
+                                <p class="text-left" style=" font-size:16px;color:#6B778C;">学歴を編集します。</p>
+                                <textarea class="border text-left" name="name" value="{{ $education->name }}"
+                                    placeholder="ex) OO大学, リハビリテーション科" cols="30" rows="10"
+                                    style="background-color:#f1f5f9;border-radius: 6px; width: 100%;">
                                     {{ $education->name }}
                                 </textarea>
 
-                            <br><button type="submit" class="btn btn-primary">編集する</button>
+                            <br><button type="submit" class="btn btn-primary" style="margin-top:40px; background-color:#2E77FD;color:white;">編集する</button>
 
                         </form>
                     </div>
