e972f19c04936cb9466c4d69d8bb665a9f50b1e1
natsukiiiii <w.natsuki.3@gmail.com>
2021-12-27 10:47:51 +0900

    auth

--- app/Http/Controllers/GoodPostController.php
+++ app/Http/Controllers/GoodPostController.php
@@ -11,6 +11,10 @@ class GoodPostController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+    public function __construct(){
+        $this->middleware('auth');
+    }
+    
     public function index()
     {
         return view('goodPosts.index');
